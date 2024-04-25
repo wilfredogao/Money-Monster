5ed0646a869568cda73837c9d9985e70fd9b9f08
wilfredogao <44207973+wilfredogao@users.noreply.github.com>
2024-04-24 23:16:21 -0400

    removed test script

--- runProgramWindows.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start cmd /k "cd /d %USERPROFILE%\Desktop\Money-Monster\money-monster\frontend && npm install && npm start"
-start cmd /k "cd /d %USERPROFILE%\Desktop\Money-Monster\money-monster\backend && npm install && npm start"
