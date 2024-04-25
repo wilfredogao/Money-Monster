c5e560f3e1cf2edcb8693475c207a0e60b598a65
wilfredogao <44207973+wilfredogao@users.noreply.github.com>
2024-04-24 23:16:35 -0400

    removed test script

--- runProgramsMacLinux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gnome-terminal --working-directory=$HOME/Desktop/Money-Monster/money-monster/frontend -- npm install && npm start &
-gnome-terminal --working-directory=$HOME/Desktop/Money-Monster/money-monster/backend -- npm install && npm start &
